9649fb07417c0c07572fc8472a14a46312b6b66a
Johnson Lin <g111095022@smail.nchu.edu.tw>
2024-07-25 11:49:33 +0800

    intergrate predictMask and evaluate function into runner

--- predict.py
+++ predict.py
@@ -1,19 +1,13 @@
 import argparse
 import os
-import cv2
-import numpy as np
-import torch
 import warnings
-from PIL import Image
 warnings.filterwarnings("ignore")
-from tqdm import tqdm
 import threading
 
 #custom
-from models import get_models
 from dataset import  GrayDataset
-from metric import compute_mIoU, dice_score, iou
 from utils import Plotter, GradCam
+from runner import TSRunner
 
 test_img_dir = r"D:\tsungyu\chromosome_data\chang\changValSum\images"
 test_truth_dir = r"D:\tsungyu\chromosome_data\chang\changValSum\masks"
@@ -27,85 +21,39 @@ def get_args():
     parser.add_argument('--normalize', action="store_true",dest="is_normalize",default=True, help='model normalize layer exist or not')
     parser.add_argument('--fix_encoder', action="store_true",default=True, help='fix encoder')
     parser.add_argument('--pad_mode', action="store_true",default=True, help='unet used crop or pad at skip connection')
-    parser.add_argument('--instanceloss', action="store_true",default=False, help='using instance seg loss during training')
+    parser.add_argument('--instanceloss', action="store_true",default=False, help='In this project, is always False')
     parser.add_argument('--in_channel','-i',type=int, default=1,help="channels of input images")
     parser.add_argument('--classes','-c',type=int,default=2,help='Number of classes')
-    parser.add_argument('--weight', '-w', default=r'log\discussion_after_test\IN_CE_exchange_input_m09999\teacher_50.pth', metavar='FILE',help='Specify the file in which the model is stored')
+    parser.add_argument('--device', type=str,default='cpu',help='use cpu or gpu')
+    parser.add_argument('--weight', '-w', default=r'log\IN_ASL_m0999_entropyweight_hardlabel\student_50.pth', metavar='FILE',help='Specify the file in which the model is stored')
     parser.add_argument('--imgdir', type=str,default=r'', help='the path of directory which imgs saved for predicting')
     parser.add_argument('--imgpath', '-img',type=str,default=r'', help='the path of img')
     parser.add_argument('--eval', action="store_true",default=True, help='calculate miou and dice score')
     
     return parser.parse_args()
 
-def predict_mask(net,imgpath:str):
-    plotter = Plotter()
-    net = net.to(device="cpu")
-    img = torch.from_numpy(cv2.imread(imgpath,cv2.IMREAD_GRAYSCALE)).unsqueeze(2).permute(2,0,1)
-    img = img.unsqueeze(0)#加入批次軸
-    img = img.to(dtype=torch.float32, device='cpu')
-    logit = net(img)
-    plotter.plot_entropy(torch.softmax(logit, dim=1),saved=True,is_heat=True)
-    mask_pred = torch.argmax(torch.softmax(logit, dim=1),dim=1,keepdim=True).to(torch.int32)
-    mask_pred = mask_pred.squeeze().numpy()
-    mask_pred = mask_pred.astype(np.uint8)*255
-    im = Image.fromarray(mask_pred)
-    im.save(f"./predict_{os.path.basename(imgpath)}")
-
-    return mask_pred
-
-def evaluate_imgs(net,
-                testdataset,):
-    # net.eval() #miou 計算不用 eval mode 因為 running mean and running std 誤差可能在訓練過程紀錄的時候過大
-    evaluation_dict = {}
-    if not evaluation_dict.get("miou"):
-        evaluation_dict["miou"] = []
-    if not evaluation_dict.get("dice"):
-        evaluation_dict["dice"] = []
-    if not evaluation_dict.get("iou"):
-        evaluation_dict["iou"] = []
-
-    for i,(img, truth) in enumerate(tqdm(testdataset)):
-        img = img.unsqueeze(0)#加入批次軸
-        img = img.to(dtype=torch.float32)
-        truth = truth.unsqueeze(0).to(dtype=torch.int64)#加入批次軸
-        #print('shape of truth: ',truth.shape)
-        with torch.no_grad():
-            logit = net(img)   
-            mask_pred = torch.argmax(torch.softmax(logit,dim=1),dim=1,keepdim=True).to(torch.int64) # (1,1,h ,w)
-            #print('shape of mask_pred: ',mask_pred.shape)
-
-            #compute the mIOU and dice score
-            miou = compute_mIoU(mask_pred.numpy(), truth.numpy())
-            f1 = dice_score(mask_pred.detach().squeeze(1), truth.detach().squeeze(1))
-            chromosome_iou = iou(mask_pred.squeeze(1), truth.squeeze(1))
-            # print(miou)
-            evaluation_dict["miou"].append(miou)
-            evaluation_dict["dice"].append(f1)
-            evaluation_dict["iou"].append(chromosome_iou)
-
-    for k in evaluation_dict:
-        evaluation_dict[k] = sum(evaluation_dict[k]) / len(evaluation_dict[k])
-
-    return evaluation_dict
-
-if __name__ == '__main__':
+def main():
     args = get_args()
     testset = GrayDataset(img_dir = test_img_dir, mask_dir = test_truth_dir)
-    net = get_models(model_name=args.model, is_cls=True,args=args)
+    runner = TSRunner(args=args)
+    runner.pretrain_model(weightPath=args.weight)
+    modelName = os.path.basename(args.weight).split("_")[0]
+    print(modelName)
 
-    print(f'Loading model {args.weight}')
-    net.load_state_dict(torch.load(args.weight, map_location="cpu",),strict=False)
-    print('Model loaded!')
     if args.imgdir:
         imgs = os.listdir(args.imgdir)
         imgs = [os.path.join(args.imgdir,i)for i in imgs]
         for i in imgs:
-            predict_mask(net=net,imgpath=i)
+            runner.inference(i, modelName=modelName, entropymap=True)
     # predict one images
     elif args.imgpath:
-        predict_mask(net=net,imgpath=args.imgpath)
+        runner.inference(args.imgpath, modelName=modelName, entropymap=True)
 
     # evaluate images
     if args.eval:
-        performance = evaluate_imgs(net=net,testdataset=testset)
-        print(f'{performance}')
+        runner.evaluate(testdataset=testset, modelName=modelName)
+
+
+if __name__ == '__main__':
+    main()
+    
--- runner.py
+++ runner.py
@@ -1,9 +1,13 @@
 import torch
-from models.losses import Distribution_loss
-from models import get_models
-from utils import set_grad, prob2entropy
+from PIL import Image
+from tqdm import tqdm
 import json
 import os
+import cv2
+from models.losses import Distribution_loss
+from models import get_models
+from utils import set_grad, prob2entropy, Plotter
+from metric import compute_mIoU, dice_score, iou
 
 
 class BaseRunner(object):
@@ -13,40 +17,96 @@ class BaseRunner(object):
         self.loss = loss
         self.optimizer = optimizer
         self._args = args
-    
-    def train(self):
-        pass
-    
-    def inference(self):
-        pass
 
     def update_param(self):
         pass
 
     def set_args(self, args:dict):
         self._args = args
+    
+    def inference(self):
+        pass
+
+    def evaluate(self):
+        pass
 
     def get_cfg(self, filename:str = 'train_cfg.json'):
         print(self._args)
         with open(filename, "w") as outfile: 
             json.dump(self._args, outfile)
 
-
+################################################################################################################
 class TSRunner(BaseRunner):
     def __init__(self, args):
         self._args = args
         self.teacher = get_models(args.model, is_cls=True,args=args)
         self.student = get_models(args.model, is_cls=True,args=args)
         self.lossfn = Distribution_loss()
+        self.device = torch.device(args.device)
         self.optimizer = None
         self.loss_val = 0
         # initialize
+        self._toDevice()
         self.set_args()
     
-    def inference(self):
-        self.teacher.eval()
-        self.student.eval()
-        pass
+    def inference(self, imgpath:str,  modelName:str = 'teacher', entropymap:bool=False):
+        '''
+        Predict segmentation mask for image
+        modelName:   teacher | student
+        '''
+        model = modelName.lower()
+        self._toDevice()
+        self._eval()
+
+        # Process data
+        img = torch.from_numpy(cv2.imread(imgpath,cv2.IMREAD_GRAYSCALE)).unsqueeze(2).permute(2,0,1)
+        img = img.unsqueeze(0) #加入批次軸
+        img = img.to(dtype=torch.float32, device=self._args.device)
+
+        if model == 'teacher':
+            mask_pred = self._predictMask(model = self.teacher, data = img, entropymap=entropymap)
+            self._saveImg(img=mask_pred, path=f"./predict_{os.path.basename(imgpath)}")
+        elif model == 'student':
+            mask_pred = self._predictMask(model = self.student, data = img, entropymap=entropymap)
+            self._saveImg(img=mask_pred, path=f"./predict_{os.path.basename(imgpath)}")
+        else:
+            raise NameError(f"modelName must be 'teacher' or 'student' .")
+        
+    def evaluate(self, testdataset, modelName:str = 'teacher'):
+        model = modelName.lower()
+        self._toDevice()
+        self._eval()
+        evaluation_dict = {}
+        if not evaluation_dict.get("miou"):
+            evaluation_dict["miou"] = []
+        if not evaluation_dict.get("dice"):
+            evaluation_dict["dice"] = []
+        if not evaluation_dict.get("iou"):
+            evaluation_dict["iou"] = []
+
+        net = self.student if model == 'student' else self.teacher
+
+        for i,(img, truth) in enumerate(tqdm(testdataset)):
+            img = img.unsqueeze(0)#加入批次軸
+            img = img.to(device=self._args.device,dtype=torch.float32)
+            truth = truth.unsqueeze(0).to(device=self._args.device,dtype=torch.int64)#加入批次軸
+            with torch.no_grad():
+                mask_pred = self._predictMask(model=net, data=img)
+                #compute the mIOU and dice score
+                miou = compute_mIoU(mask_pred.numpy(), truth.numpy())
+                f1 = dice_score(mask_pred.detach().squeeze(1), truth.detach().squeeze(1))
+                chromosome_iou = iou(mask_pred.squeeze(1), truth.squeeze(1))
+                # print(miou)
+                evaluation_dict["miou"].append(miou)
+                evaluation_dict["dice"].append(f1)
+                evaluation_dict["iou"].append(chromosome_iou)
+
+        for k in evaluation_dict:
+            evaluation_dict[k] = sum(evaluation_dict[k]) / len(evaluation_dict[k])
+
+        print(evaluation_dict)
+
+        return evaluation_dict
 
     def update_param(self, dataForTeacher, dataForStudent, needWeight:bool=False):
         logits, labels, weights = self._forward(dataForTeacher, dataForStudent, needWeight)
@@ -58,16 +118,19 @@ class TSRunner(BaseRunner):
         self._update_teacher_param(self._args.momentum) # update teacher
 
     def set_args(self, args= None):
-        self._args = args if args is not None else self._args
-        self.loss_val = 0
-        self.optimizer = torch.optim.Adam(self.student.parameters(), lr = self._args.init_lr,betas=(0.9,0.999))
-        self.lossfn.set_metric(self._args.loss)
-        self.pretrain_model(self._args.loadpath)
-        set_grad(model=self.teacher, is_requires_grad=False)
-        self.teacher.to(self._args.device)
-        self.student.to(self._args.device)
+        try:
+            self._args = args if args is not None else self._args
+            self.loss_val = 0
+            self.optimizer = torch.optim.Adam(self.student.parameters(), lr = self._args.init_lr,betas=(0.9,0.999))
+            self.lossfn.set_metric(self._args.loss)
+            self.pretrain_model(self._args.loadpath)
+            set_grad(model=self.teacher, is_requires_grad=False)
+            self.device = torch.device(self._args.device)
+        except:
+            pass
 
     def pretrain_model(self, weightPath = None):
+        '''Load the pretrained weight into the teacher and student model'''
         if (weightPath is None):
              return
         pretrained_model_param_dict = torch.load(weightPath)
@@ -86,11 +149,34 @@ class TSRunner(BaseRunner):
     def save_weight(self, saveFolder, suffix):
         torch.save(self.student.state_dict(), os.path.join(saveFolder,f'student_{suffix}.pth'))
         torch.save(self.teacher.state_dict(), os.path.join(saveFolder,f'teacher_{suffix}.pth'))
+
+    def _eval(self):
+        self.teacher.eval()
+        self.student.eval()
+        
+    def _predictMask(self, model, data, entropymap:bool=False):
+        logit = model(data)
+        prob = torch.softmax(logit, dim=1)
+        if entropymap:
+            plotter = Plotter()
+            plotter.plot_entropy(prob,saved=True,is_heat=True)
+
+        mask_pred = torch.argmax(prob,dim=1,keepdim=True).to(torch.int64)
+        
+        return mask_pred # (1, 1, h, w)
+        
+    def _saveImg(self, img, path):
+        '''img: torch.Tensor'''
+        img = img.squeeze().to(torch.uint8).numpy()
+        img = img*255
+        im = Image.fromarray(img)
+        im.save(path)
     
     def _forward(self, dataForTeacher, dataForStudent, needWeight:bool=False):
         weight = None
         self.teacher.train()
         self.student.train()
+        self._toDevice()
         dataForTeacher = dataForTeacher.to(device=self._args.device,dtype=torch.float32)
         dataForStudent = dataForStudent.to(device=self._args.device,dtype=torch.float32)
         # Generate pseudoLabel
@@ -117,3 +203,7 @@ class TSRunner(BaseRunner):
             for param_t in self.teacher.named_parameters():
                 if param_t[0] in student_name_parameters.keys():
                     param_t[1].data = param_t[1].data.mul_(momentum).add_((1-momentum)*student_name_parameters[param_t[0]])
+
+    def _toDevice(self):
+        self.teacher.to(self._args.device)
+        self.student.to(self._args.device)
